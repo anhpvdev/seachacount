a517e446bb3b9dba30655c92d26393533ef4a800
anhpvdev <anhpv.dev@gmail.com>
2024-08-22 08:38:58 +0700

    fix cors

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 require('dotenv').config();
 const express = require('express');
 const app = express();
+const cors = require('cors');
 const roleRoutes = require('./routes/roleRoutes');
 
 const accountRoutes = require('./routes/acountRouter');
 
+app.use(cors());
 app.use(express.json());
 
 app.use((req, res, next) => {
